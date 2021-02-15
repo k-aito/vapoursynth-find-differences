fe16a0f78e131eba41dadd9c64cf0f7b7ccfdda9
k-aito <77667659+k-aito@users.noreply.github.com>
2021-02-15 16:56:09 +0100

    Update and rename vapoursynth-find-black-transition.py to vapoursynth-find-differences.py
    Add feature to compare 2 videos

--- vapoursynth-find-differences.py
+++ vapoursynth-find-differences.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-#  vapoursynth-find-black-transition.py
+#  vapoursynth-find-differences.py
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -21,10 +21,10 @@
 #
 
 """
-Vapoursynth-find-black-transition.py can be used to find where there are black frames in a video
-- Compare each frames to a black frames
+Vapoursynth-find-differences.py can be used to find where there are black frames in a video
+- Compare each frames to another one
 - If the difference is lower than the percent (0.1 by default) we keep the frame in memory
-- It display the intervals of all the frames at end 
+- It display the intervals of all the frames at end
 """
 
 ################
@@ -39,15 +39,21 @@ parser.add_argument("-s", "--source",
                     required=True,
                     help="Paths of the video")
 
+# OPTIONAL: -d / --diff
+parser.add_argument("-d", "--diff",
+                    required=False,
+                    help="Video that will be used for comparison if they have the same amount of frames (default: black blankclip)")
+
 # OPTIONAL: -p / --percent
 parser.add_argument("-p", "--percent",
                     required=False,
-                    help="Percent of differnce between video and black frames, 0 minimum and 1 maximum (default 0.1)")
+                    help="Percent of difference between video and another frames, 0 minimum and 1 maximum (default 0.1), "
+                         + "if we compare to another video we display the frames that are different of 0 instead of lower to the percent")
 
 # OPTIONAL: -v / --verbose
 parser.add_argument("-v", "--verbose",
                     required=False,
-                    help="Enable the progression while scanning each frames (default: disabled)",
+                    help="Enable debug and progression while scanning each frames (default: disabled)",
                     action='store_true')
 
 args = parser.parse_args()
@@ -63,23 +69,38 @@ v = core.ffms2.Source(args.source)
 
 sceneFrames = []
 
-black = core.std.BlankClip(v, keep=1)
-diff = core.std.PlaneStats(v, black)
+if args.diff == None:
+  black = core.std.BlankClip(v, keep=1)
+  diff = core.std.PlaneStats(v, black)
+
+  # Define args.percent if not defined
+  if args.percent == None:
+    args.percent = 0.1
+else:
+  v2 = core.ffms2.Source(args.diff)
+  # Check that the amount of frames are the same if not quit
+  if v.num_frames == v2.num_frames:
+    diff = core.std.PlaneStats(v, v2)
+  else:
+    print("ERROR: the amount of frames are not the same (v1: {} / v2: {})".format(v.num_frames, v2.num_frames))
+    import os
+    os._exit(1)
+
 fps = diff.fps.numerator / diff.fps.denominator
 
-# Define args.percent if not defined
-if args.percent == None:
-  args.percent = 0.1
-
-print("INFO: Start the comparaison of each frames to a black frame")
+print("INFO: Start the comparison of each frames")
 for i in range(0, diff.num_frames):
   if args.verbose:
-    print('{} / {}'.format(i, diff.num_frames))
+    print('{} / {} [PlaneStatsDiff: {}]'.format(i, diff.num_frames, diff.get_frame(i).props['PlaneStatsDiff']))
   else:
     if i % 1000 == 0:
       print("PROGRESS: Compare {} on {}".format(i, diff.num_frames))
-  if diff.get_frame(i).props['PlaneStatsDiff'] < float(args.percent):
-    sceneFrames.append(i)
+  if args.diff:
+    if diff.get_frame(i).props['PlaneStatsDiff'] != 0.0:
+      sceneFrames.append(i)
+  else:
+    if diff.get_frame(i).props['PlaneStatsDiff'] < float(args.percent):
+      sceneFrames.append(i)
 print("")
 
 sceneSlices = []
@@ -109,7 +130,7 @@ for i,value in enumerate(sceneFrames):
     # Actual value is endSlice
     endSlice = value
 
-    # Method to conver seconds in hh:mm:ss
+    # Method to convert seconds in hh:mm:ss
     # https://bbs.archlinux.org/viewtopic.php?id=77634
     def humanize_time(secs):
       mins, secs = divmod(secs, 60)
@@ -134,6 +155,6 @@ for i,value in enumerate(sceneFrames):
     # Reset startSlice
     startSlice = -1
 
-print("INFO: Intervals that are maybe black transitions")
+print("INFO: Intervals that maybe have differences")
 import pprint
 pprint.pprint(sceneSlices)
